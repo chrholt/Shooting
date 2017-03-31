8e4599ec845a4e873ec1c913457349a9c19469ae
Christoffer Holt <chholt93@gmail.com>
2017-03-31 17:51:10 +0200

    first

---
+++ Shooting/Shooting/Jaktfelt.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Shooting
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Jaktfelt : ContentPage
+    {
+        public Jaktfelt()
+        {
+            InitializeComponent();
+        }
+
+        private void Register_Jaktfelt(object sender, EventArgs e)
+        {
+            DisplayAlert("SUCCESS", "Successfully registered results", "GREAT!");
+        }
+    }
+}
---
+++ Shooting/Shooting/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Shooting
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Jaktfelt_page(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Jaktfelt());
+        }
+        private async void Figurjakt_page(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Figurjakt());
+        }
+        private async void Jegertrap_page(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Jegertrap());
+        }
+    }
+}
