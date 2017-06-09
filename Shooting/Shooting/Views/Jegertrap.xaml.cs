8fc8840bef95973b5d782f54004b8a2756babbd8
Christoffer Holt <chholt93@gmail.com>
2017-06-09 22:45:57 +0200

    Started designing jegertrap page and functionality

--- Shooting/Shooting/Views/Jegertrap.xaml.cs
+++ Shooting/Shooting/Views/Jegertrap.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Shooting.Database;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +14,42 @@ namespace Shooting
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Jegertrap : ContentPage
     {
+        private ShootingDatabase database;
+        private ObservableCollection<Result> jegertrapResults;
+
         public Jegertrap()
         {
             InitializeComponent();
+            this.database = new ShootingDatabase();
+
+            ToolbarItems.Add(new ToolbarItem
+            {
+                Icon = "plus_round_128x128_white_hollow.png",
+                Text = "New Result",
+                Command = new Command(this.GoToRegisterJegertrapResult)
+
+            });
+            jegertrapResults = database.GetFigurjaktResults();
+            jegertrapResultsListView.ItemsSource = jegertrapResults;
+        }
+
+        private void jegertrapResultsListView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var selectedItem = (Result)e.SelectedItem;
+            //var newPage = new JegertrapDetails(selectedItem, jegertrapResults);
+            //newPage.BindingContext = selectedItem;
+            //Navigation.PushAsync(newPage);
+        }
+
+        private void GoToRegisterJegertrapResult()
+        {
+            //var newPage = new JegertrapCreate(jegertrapResults);
+            //Navigation.PushAsync(newPage);
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
         }
     }
 }
